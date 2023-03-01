12290f944f0dd28a1a25473f1d95ff06f33f24a4
Gatwan <abigailgathigia@gmail.com>
2023-03-01 09:22:01 +0300

    Fix code error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-		
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-	return 1 + (lh > rh ? lh : rh);
+
+	lh = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+	rh = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
+	return (lh > rh ? lh : rh);
 }
